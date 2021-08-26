bc82385b8113ca2be9969cd1221ea1908728c43c
EUNHEE JANG <6810779s@naver.com>
2021-08-26 10:59:42 +0900

    add container

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Container.module.css';
+
+const Container = () => {
+  return (
+    <div className={styles.container}>
+      <top className={styles.title}>
+        <h1>Planner</h1>
+      </top>
+    </div>);
+};
+
+export default Container;
