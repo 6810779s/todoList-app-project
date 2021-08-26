5444a9c1f06a47afd2e4b789cf7d0668c273844b
EUNHEE JANG <6810779s@naver.com>
2021-08-26 19:06:46 +0900

    check styling

--- src/components/AddLists.js
+++ src/components/AddLists.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/AddLists.module.css';
 import List from './List';
 
 const AddLists = () => {
+  const [lists, setLists] = useState([
+    { id: 1, text: '', checked: false },
+    { id: 2, text: '', checked: true },
+  ]);
+  const [nextId, setNextId] = useState(2);
   return (
     <div className={styles.container}>
-      <List />
-      <List />
-      <List />
+      {lists.map((list) => (
+        <List list={list} key={list.id} />
+      ))}
     </div>
   );
 };
--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import styles from '../styles/List.module.css';
+import classNames from 'classnames/bind';
+
 import {
   MdCheckBoxOutlineBlank,
+  MdCheckBox,
   MdRemoveCircleOutline,
   MdCreate,
 } from 'react-icons/md';
 
-const List = () => {
+const cx = classNames.bind(styles);
+
+const List = ({ list }) => {
+  const { text, checked } = list;
   return (
     <div className={styles.items_container}>
-      <div className={styles.checkbox}>
-        <MdCheckBoxOutlineBlank />
+      <div className={cx('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className={styles.text}>list</div>
       </div>
       <div className={styles.function}>
-        <MdRemoveCircleOutline />
-        <MdCreate />
+        <div className={styles.remove}>
+          <MdRemoveCircleOutline />
+        </div>
+        <div className={styles.modify}>
+          <MdCreate />
+        </div>
       </div>
     </div>
   );
